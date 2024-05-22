763a88585a95b58431c87aff6c2b0ea7a5ffbe3c
Michalis Anastasiou <mihalosnst4@gmail.com>
2024-05-22 20:23:38 +0300

    jQuery Intruduction

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-$("h1").css("color","red");
+$("h1").css("color", "red");
+$("h1").addClass("big-title");
+//$("h1").removeClass("big-title");
+$("h1").text("Bye");
+$("button").html("<em>Nooo</em>");
+
+$("h1").click(function () {
+  $("h1").css("color", "purple");
+});
+
+// $("button").click(function(){
+//     $("h1").css("color","green");
+// });
+
+$(document).keypress(function (e) {
+  $("h1").text(e.key);
+});
+
+$("h1").mouseover(function () {
+  $("h1").css("color", "lightblue");
+});
+
+$("h1").before("<button>Before</button>");
+
+// $("button").remove();
+
+$("button").on("click", function () {
+  $("h1").toggle();
+});
